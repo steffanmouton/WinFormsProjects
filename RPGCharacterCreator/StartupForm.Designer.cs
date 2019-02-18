0c28e15baede301610c278798448eea73bd99e87
steffanmouton <36455372+steffanmouton@users.noreply.github.com>
2019-02-18 00:10:47 -0600

    Added a startup window with character select for editing

---
+++ RPGCharacterCreator/StartupForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RPGCharacterCreator
+{
+    partial class StartupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(274, 303);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(294, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This is the RPG Character Creator.\r\nEither choose a character on the left to edit" +
+    ", or select \'New Character\' to create a new one.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 76);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Edit Existing";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(549, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 76);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "New Character";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // StartupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 334);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "StartupForm";
+            this.Text = "StartupForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
