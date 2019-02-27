676a97c3f6fa9be9e33c9c35cd98398d16ae545c
Steffan Mouton <smouton8993@gmail.com>
2019-02-27 11:06:45 -0600

    Revert "Moved files into folders"
    This reverts commit aa102475b33c3a950bf63a5ad677fff42789fbb1.

---
+++ RPGCharacterCreator/Forms/StartupForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPGCharacterCreator
+{
+    public partial class StartupForm : Form
+    {
+        public StartupForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Clicking the new character button opens the Race Class Name Form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void newCharButton_Click(object sender, EventArgs e)
+        {
+            var raceForm = new RaceClassNameForm();
+            raceForm.Show();
+            this.Hide();
+        }
+    }
+}
