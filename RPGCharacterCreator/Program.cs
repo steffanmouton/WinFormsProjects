b6034310326782446edc073fea0d6c41efa6031c
Steffan Mouton <smouton8993@gmail.com>
2019-03-25 09:53:34 -0500

    Small code cleanup. Removed unused "using" files

--- RPGCharacterCreator/Program.cs
+++ RPGCharacterCreator/Program.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
-using Newtonsoft.Json;
-using RPGCharacterCreator.UI;
 
 namespace RPGCharacterCreator
 {
